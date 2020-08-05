150e0b1af47b30f29e888ad4ec33093cce0fa735
comanchechao <61477751+comanchechao@users.noreply.github.com>
2020-08-05 23:48:08 +0330

    Starting to transfer some code

---
+++ accounts/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+        def save(self, commit=True):
+            user = super(SignupForm, self).save(commit=False)
+            user.first_name = cleaned_data['first_name']
+            user.last_name = cleaned_data['last_name']
+            user.email = cleaned_data['email']
+
+            if commit:
+                user.save()
+
+                return user
