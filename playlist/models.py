a5573fd8f30320fc10818f0184c83cf4b73f3539
comanchechao <61477751+comanchechao@users.noreply.github.com>
2020-08-21 16:07:28 +0330

    Update models.py

--- playlist/models.py
+++ playlist/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Playlist(models.Model):
     title = models.CharField(max_length=200, help_text="Enter a name for your playlist")
@@ -6,6 +7,19 @@ class Playlist(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     thumbnail = models.ImageField(default='default.png', blank=True)
     creater = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+
+class Tracks(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, help_text='Enter a music genre (e.g. Psychedelic Rock)')
+
+    def __str__(self):
+        return self.name
