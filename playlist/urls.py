d5056a82df3682bc426e488febf69b4d1cb759d8
comanchechao <61477751+comanchechao@users.noreply.github.com>
2020-08-24 08:59:38 +0330

    playlist list page start

--- playlist/urls.py
+++ playlist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 app_name = 'playlist'
 urlpatterns = [
-
+    url(r'^$', views.playlist_list, name="list"),
 ]
--- playlist/views.py
+++ playlist/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from playlist.models import Playlist, Genre, Artist, Album, Song
 
-# Create your views here.
+def playlist_list(request):
+    playlists = Playlist.objects.all().order_by('date');
+    context = {
+    'playlists': playlists,
+    }
+    return render(request, 'playlist/index.html', context=context)
