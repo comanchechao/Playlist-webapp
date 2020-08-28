aea219c84d255a2c7bc82dd6b2016a207629d64a
comanchechao <61477751+comanchechao@users.noreply.github.com>
2020-08-29 00:32:57 +0330

    Signup Page Complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,15 +3,24 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required')
+    email = forms.EmailField(max_length=200, help_text='Required', label='', label_suffix='', widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'E-Mail'}
+        ))
+    username = forms.CharField(max_length=200, label='', label_suffix='', widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Username'}
+        ))
+    password1 = forms.CharField(max_length=200, label='', label_suffix='',widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Password'}
+        ))
+    password2 = forms.CharField(max_length=200, label='', label_suffix='', widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Retype Password'}
+        ))
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
 
         def save(self, commit=True):
             user = super(SignupForm, self).save(commit=False)
-            user.first_name = cleaned_data['first_name']
-            user.last_name = cleaned_data['last_name']
             user.email = cleaned_data['email']
 
             if commit:
