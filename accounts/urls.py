c6269a6444bee16eb96158e3e866ca0a7a985a41
comanchechao <61477751+comanchechao@users.noreply.github.com>
2020-09-08 23:15:43 +0330

    New Homepage start

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
 
     url(r'^signup/$', views.SignupView, name="signup"),
     url(r'^login/$', views.LoginView, name="login"),
+    url(r'^logout/$', views.LogoutView, name="logout"),
+
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from .forms import SignupForm, LoginForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
 
 
 def SignupView(request):
@@ -24,8 +25,14 @@ def LoginView(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('hompage.html')
+            return redirect('/templates/homepage.html')
 
     else:
         form = LoginForm()
     return render(request, 'accounts/login.html', { 'form': form })
+
+
+def LogoutView(request):
+    if request.method == 'POST':
+            logout(request)
+            return redirect('templates/homepage.html')
